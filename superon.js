9e7f44c0090fcfa60e692354f7dc334ec4313c7b
Leon <info@leon.vankammen.eu>
2021-01-26 19:33:38 +0100

    work in progress [might break]

--- superon.js
+++ superon.js
@@ -1,230 +1,71 @@
-function on(obj){
+let clone       = (o) => o.slice ? o.map( (e) => typeof e == 'object' ? clone(e) : e )
+								 : Object.assign({}, o)	
+var on
+on = function on(o,f){
+	let so  = on.so
+    let to  = 'object'
+	if( o == '*' ) return so.subs.push(f) 
 
-    let pad         = (value, length) => (value.toString().length < length) ? pad(value+" ", length) : value;
-    let ls          = () => console.table(this.dump())
-    let clone       = (o) => Object.assign({}, o)
+    so.on = function(f){
+	  let type
+	  if( typeof f == 'object' && f.slice ) {
+	    f = function(args, o){ return on.pipe.apply(this, args)(o) }.bind(this, Array.prototype.slice.call(arguments) )
+		type = 'o'
+	  }
+	  type = type || String(f.toString().match(/(,i|i|o)\) /)||[])[0]
+	  let bak  = {}
+	  this.inputs  = this.inputs  || []
+	  this.outputs = this.outputs || []
+	  if( type == 'i' ) this.inputs.push(f)
+	  if( type == 'o' ) this.outputs.push(f)
+	  if( this.par[ this.i ].on.remove ) return // already wrapped
 
-    let api = {
-
-        error: (o) => (msg) => console.error("superon: "+msg), 
-	
-        serialize: (o) => () => o.rules, 
-
-        load: (o) => (i, when) => {
-            let add = (rule) => {
-              let cb = function(rule, input){
-                  if( when(input,rule) ) rule.then.map( (t) => {
-                      if( !this.get(t.f) ) return this.on.error("function "+t.f+" does not exist :/")
-                      this.get(t.f)(input, t.opts) 
-                  })
-              }.bind(o, rule)
-              rule.exprlang = when
-              cb.rule = rule
-              o.on(rule.on, cb)
-            }
-            if( typeof i == 'object' && !i.slice ) add(i)
-            else i.map( add )
-        }, 
-
-        remove: (o) => () => {
-            if( !o.rules ) return
-            o.rules = o.rules.filter( (o) => o.remove() )
-            o.rules = []
-        }, 
-
-        dump: (o) => () => {
-            let depth = ''
-            let out   = []
-            let functions = o.flatten(o, {hide:Object.keys(_).concat(['on',/rules.*/])})
-            let then2str = (t) => t.f+"("+( t.opts ? JSON.stringify(t.opts).replace(/^{/,'{') : '' )+')' 
-            let len = 30
-            for ( var i in functions ) {
-                out.push({on:pad(i, len)})
-                o.rules.map( (rule) => {
-                    if( !rule.on.replace(/:.*/,'').match(i) ) return
-                    let then = rule.then
-                    then.map( (t) => {
-                        let r = Object.assign({},rule)
-                        r.on = pad(r.on+(r.type||''),len)
-                        r.then = pad( then2str( t ), len)
-                        delete r.remove
-                        delete r.type
-                        delete r.exprlang
-                        out.push( r )
-                    })
-                })
-            }
-            return out
-        }, 
-
-        ls:        (o) => () => console.table( o.on.dump() ), 
-        dot:       (o) => () => {
-            let nodes = []
-            o.rules.map( (r) => {
-                r.then.map( (t) => {
-                    let s = `\t\t"${r.on.replace(/:.*/,'')}" -> "${ t.f + (o.get(t.f)?'':' ?!?') }" [ label = "${r.type.replace(/:/,'')}" ]`
-                    nodes.push(s)
-                })
-            })
-            return `digraph finite_state_machine {
-                rankdir=LR;
-                size="10"
-                node [shape = circle];
-
-a{nodes.join("\n")}
-            }`
-        }
+	  // wrap the original function
+	  if( type == ',' ) this.par[ this.i ].wrap = f
+	  for( var i in this ) bak[i] = this[i]
+	  this.par[ this.i ] = function(orig,wrap,inputs,outputs,i){
+		let o   = {}
+		i.clone = () => clone(i)
+		try{
+			inputs.map( (f) => f(i) )
+			o = orig(i)
+			outputs.map( (f) => o = f(o) || o )
+		}catch(e){ o = {error:e,input:i,func:this.i}}
+		so.subs.map( (s) => { try{ s(i,o) }catch(e){ o = {error:e,func:this.i,input:i,output:o} } })
+		return o 
+	  }.bind(this.par, this, this.wrap, this.inputs,this.outputs)
+	  for( var i in bak ) 
+		if( i != this.i ) this.par[ this.i ][i] = bak[i]
+	  this.par[ this.i ].on.remove = ((orig) => () => this.par[this.i] = orig )(this)
+	  so.handlers.push( this.par[ this.i ] )
+      return so
     }
-
-    // here comes the super on function
-
-    let on = function(){
-        let args   = Array.prototype.slice.apply(arguments)
-        let fn     = String(args[0]).replace(/:.*/, '').split(" ")[0]
-        this.rules = this.rules || []
-        var orig     = this.get(fn)
-        var is_async = (f) => _.get(f, 'constructor.name') == 'AsyncFunction'
-        var thens   = args.slice(1)
-        var rule
-        if( thens[0].rule ) rule = thens[0].rule
-        else {
-            rule        = {}
-            rule.then   = thens.map( (f) => ({f:f.path || f.name}) )
-            rule.id     = String(new Date().getTime()).substr(-5)
-        }
-        rule.on     = fn
-        rule.remove = () => this[fn] = orig
-        rule.type   = (String(args[0]).match(/:(input|output)/) || [])[0] || ':wrap'
-        
-        var fnew = function(){
-            let out
-
-            // prepare inputs
-            let input = Array.prototype.slice.apply(arguments)
-            let runHooks = (input) => {
-                for( var i in thens ){
-                    let f = thens[i]
-                    if( f ) f.apply(this, input)
-                }
-            }
-            // run when 
-            let when = !rule.when || (rule.when && rule.exprlang && rule.exprlang(input[0],rule) )
-
-            // run input hooks
-            if( when && rule.type == ":input"  ) runHooks( clone(input) )
-
-            // run the original function
-            if( when && rule.type == ':wrap'   ) out = thens[0].apply(this, [orig].concat(input) )
-            else out = orig.apply(this, input)
-            
-            let clonedout = typeof out == 'object' ? clone(out) : out
-            if( typeof out == 'object' ) out.input = () => input
-            if( when && rule.type == ":output" ) runHooks([ clonedout ])
-            return out 
-        }
-        fnew.rule = rule 
-        for( var i in this[fn] ) 
-            if( i != "rule" ) fnew[i] = this[fn][i] // preserve properties if any
-        this.set(fn, fnew )
-        this.rules.push(rule)
-        return {
-            when: function(rule,trigger,exprlang){
-                rule.when = trigger
-                rule.exprlang = exprlang
-            }.bind(this, rule), 
-            remove: function(id){ 
-                this.rules = this.rules.filter( (o) => this.id == rule.id ? this.remove() : o)
-            }.bind(this, rule)
-        }
-    }
-
-	let _ = (o, opts) => {
-	  o = o || {}
-	  opts = opts || []
-	  if( o.on ) return o // already done
-	  for( var i in _ ) o[i] = _[i].bind( o )
-      o.on = on.bind(o)
-      for ( var i in api ) o.on[i] = api[i](o)
-      o.rules = o.rules || []
-	  return o
+	so.reg = (par) => { // parent, variable path
+		for( var i in par ){
+			if( i != "on" && String(typeof par[i]).match(/(function|object)/) ) so.reg(par[i])
+			par[i].par = par
+			par[i].i  = i
+			par[i].on = so.on.bind(par[i])
+		}	
 	}
-		
-	_.undo = function(){ 
-		for ( var i in _ ) delete this[i]
-		return this
-	}
-
-	_.set = function set(path, value) {
-		var last
-		var o = this
-		path = String(path)
-		var vars = path.split(".")
-		var lastVar = vars[vars.length - 1]
-        if( typeof value == 'function' ) value.path = path 
-		vars.map(function(v) {
-			if (lastVar == v) return
-			o = (new Function("o","return o." + v)(o) || new Function("o","return o."+v+" = {}")(o))
-			last = v
-		})
-        try {
-            new Function("o","v","o." + lastVar + " = v")(o, value)
-        } catch (e) { console.error("could not set "+path) }
-		return o
-	}
-
-	// borrowed from [bless.js](https://gist.github.com/coderofsalvation/61dd5c86b81e4ca6963ed0535bd806e7)
-	_.get = function get(x,fallback) {
-	  var obj = this
-	  var o = String(x).split('.').reduce(function(acc, x) {
-		  if (acc == null || acc == undefined || acc == ''){
-			  return fallback;
-		  }
-		  return new Function("x","acc","return acc['" + x.split(".").join("']['") +"']" )(x, acc) || fallback
-	  }, obj)
-	  if( !o && fallback ) return _.set.call(obj,x,fallback)
-	  return o
-	}
-	
-	_.flatten = function(arr,opts){
-		opts = opts || {}
-		separator = opts.separator || '.'
-		function dive(currentKey, into, target) {
-			for (var i in into) {
-				if (into.hasOwnProperty(i)) {
-					var newKey = i;
-					var newVal = into[i];
-					
-					if (currentKey.length > 0) {
-						newKey = i.match(/[0-9]/) ? currentKey + "["+i+"]"
-												  : currentKey + separator + i;
-					}
-					
-					if (typeof newVal === "object") {
-						dive(newKey, newVal, target);
-					} else {
-						target[newKey] = newVal;
-					}
-				}
-			}
-		}
-		var newObj = {};
-		dive("", arr, newObj);
-		if( opts.hide ){
-			for( var i in newObj )
-                if( opts.hide.includes(i) || ( opts.type && typeof newObj[i] != opts.type ) )
-                    delete newObj[i]
-			opts.hide.map( (h) => {
-				if( typeof h != 'object' ) return
-				for( var i in newObj ) if( String(i).match(h) ) delete newObj[i]
-			})
-		}
-		return newObj;
-	}
-
-	_(obj)
-
+	so.reg(o)
+	o.on = so.on.bind(o)
 }
-        
+
+
+on.pipe = function(fns) {
+  return function(item) {
+ 	   return fns.reduce(function(prev,  fn) {
+ 			  return fn(prev);
+ 	   },  item);
+  }
+}
+on.so     = {subs:[],handlers:[]}
 on.server = typeof window == 'undefined'
+on.remove = () => {
+	on.so.handlers.map( (h) => h.on.remove() )
+	on.so.handlers = []
+}
 
 module.exports = on
+
--- test/superon.js
+++ test/superon.js
@@ -1,290 +1,150 @@
 let test = require('./test')
 let on   = require('./../superon')
+let pipe = on.pipe
 var t    = new test({title:__filename})
-var jsonLogic = require('json-logic-js')
 var filtrex  = require('filtrex').compileExpression
 // https://www.npmjs.com/package/safe-evaluate-expression
 
 let compare = (a, b) => JSON.stringify(a) == JSON.stringify(b)
-let testlog  = ""
 
-// lets immediately use superon for own tests :)
-on(console)
-console.on('log:input', function(msg){
-	testlog += msg+"\n"
+let mock = () => ({
+	foo: {
+		bar: (i) => ({...i, bar:1})
+	}
 })
 
-t.add("test unnested",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i; }
+t.add('test handlers', (next,error) => {
+	let plug = mock()
+	let clone
 
-	on(this)
-	this.on('foo', (foo, input) => {
-		input.x += 2	
-		return foo(input)
-	})
-	let out = this.foo(input)
-	if( !compare(out, {x:3}) || log != "3foo" )
-		return error("not ok:"+log+out)
+	on(plug)                                        // init
+	plug.foo.bar.on( (i) => i.a = 1               ) // add input handler 
+	plug.foo.bar.on( (o) => clone = o.clone()     ) // add output handler 
+	plug.foo.bar.on( (o) => ({...o,b:1})          ) // add output handler 
+	plug.foo.bar.on( (o) => ({...o,c:1})          ) // add output handler 
 
-	this.on.remove()
-	out = this.foo({x:1})
-	if( !compare(out, {x:1}) ) error('not removed')
+	let out = plug.foo.bar({x:1})
 
+	if( !compare({x:1,a:1,b:1,c:1}, out) ) return error("output not ok")
+	if( !compare({x:1,a:1},       clone) ) return error("clone not ok")
 	next()
 })
 
-t.add("test nested",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i; }
-	this.foo.bar     = (i) =>     { log+=i.x+"bar"; return i; }
-	this.foo.bar.baz = function(i){ log+=i.x+"baz"; return i; }
+t.add('test remove handlers', (next,error) => {
+	let plug = mock()
+	let clone
 
-	on(this)
-	this.on('foo.bar.baz', (foo, input) => {
-		input.x += 5	
-		return foo(input)
-	})
-	let out = this.foo.bar.baz(input)
+	on(plug)                                        // init
+	plug.foo.bar.on( (i) => i.a = 1               ) // add input handler 
+	plug.foo.bar.on( (o) => clone = o.clone()     ) // add output handler 
+	plug.foo.bar.on( (o) => ({...o,b:1})          ) // add output handler 
+	plug.foo.bar.on( (o) => ({...o,c:1})          ) // add output handler 
 
-	if( !compare(out, {x:6}) || log != "6baz" )
-		return error("not ok:"+log+out)
+	// remove single
+	plug.foo.bar.on.remove()
+	let out = plug.foo.bar({x:1})
+	if( out.c ) return error("c should not be there")
 
-	this.on.remove()
-
+	//remove all
+	on.remove(plug)
+	out = plug.foo.bar({x:1})
+	if( !compare({x:1,bar:1},out) ) return error("handlers not removed")
 	next()
 })
 
-t.add("test nested filtrex rule",  async (next, error) => {
+t.add('test bus / middleware', (next,error) => {
+	let plug = mock()
+	let called = false
 
-	// testdummies
-	var input  = {x:1}
-	testlog    = "" 
+	on(plug)                                        // init
 
-	this.set('foo.bar.baz'  , function(i){ return i; } )
-	this.set('haspaid'      , (i) => true              )
-
-	let dofiltrex = (input, rule) => filtrex(rule.when, {extraFunctions:this} )(input)
-
-	on(this)
-
-	this
-	.on('foo.bar.baz', (foo, input) => {
-		input.x += 5	
-		return foo(input)
+	on( '*', (i,o) => {
+		if( i && o ) called = true
 	})
-	.when('x == 1 and haspaid()', dofiltrex ) // string could originate from db
-		
-	let out = this.foo.bar.baz(input)
-	if( input.x !=6 ) return error("x is not mutated to 6")
-	out = this.foo.bar.baz(input)
-	if( input.x !=6 ) return error("x is mutated to >6")
-
-	this.on.remove()
-
-	next()
-})
-
-t.add("test input clone",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i; }
-	this.foo.bar     = (i) =>     { log+=i.x+"bar"; return i; }
-	this.foo.bar.baz = function(i){ log+=i.x+"baz"; return i; }
-
-	on(this)
-	this.on('foo.bar.baz:input', (input) => {
-		input.x += 5	
-		log += "clone"+input.x+","
-	})
-	let out = this.foo.bar.baz(input)
-	if( log != "clone6,1baz" ) return error("something wrongg: "+log)
 	
-	this.on.remove()
+	plug.foo.bar.on( (i) => i.a = 1               ) // add at least one handler
+
+	let out = plug.foo.bar({x:1})
+
+	if( !called ) return error("not called :(")
 
 	next()
 })
 
-t.add("test input clone multiple",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i; }
-	this.foo.bar     = (i) =>     { log+=i.x+"bar"; return i; }
-	this.foo.bar.baz = function(i){ log+=i.x+"baz"; return i; }
 
-	on(this)
-	this.on('foo.bar.baz:input', (input) => {
-		input.x += 5	
-		log += "clone"+input.x+","
-	},  this.foo.bar)
-	let out = this.foo.bar.baz(input)
-	if( log != "clone6,6bar1baz" ) return error("something wrongg: "+log)
-	
-	this.on.remove()
+t.add('test throw error', (next,error) => {
+	let plug = mock()
+	let called = false
 
-	next()
-})
+	on(plug)                                 // init
 
-t.add("test output clone",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i;     }
-	this.foo.bar     = (i) =>     { log+=i.x+"bar"; return i;     }
-	this.foo.bar.baz = function(i){ log+=i.x+"baz"; return {y:5}; }
-
-	on(this)
-	this.on('foo.bar.baz:output', (input) => {
-		log += "clone"+input.y
+	on( '*', (i,o) => {
+		if( o.error ) called = true
 	})
-	let out = this.foo.bar.baz(input)
-	if( log != "1bazclone5" ) return error("something wrong: "+log)
 	
-	this.on.remove()
+	plug.foo.bar.on( (i) => { throw "BAM!"} ) // add at least one handler
+
+	let out = plug.foo.bar({x:1})
+
+	if( !called ) return error("not called :(")
 
 	next()
 })
 
-t.add("test output multiple",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i;     }
-	this.foo.bar     = (i) =>     { log+=i.x+i.y+"bar"; return i;     }
-	this.foo.bar.baz = function(i){ log+=i.x+"baz"; return {y:5, x:i.x}; }
+t.add('test pipe', (next,error) => {
+	let plug = mock()
+	let called = false
 
-	on(this)
-	this.on('foo.bar.baz:output', (input) => {
-		log += "clone"+input.y
-		input.y += 1
-	},  this.foo.bar )
-	let out = this.foo.bar.baz(input)
-	if( log != "1bazclone57bar" ) return error("something wrong: "+log)
-	
-	this.on.remove()
+	on(plug)                                 // init
 
-	next()
-})
-
-t.add("test superon in my superon",  async (next, error) => {
-	// testdummies
-	var input  = {x:1}
-	var log    = "" 
-	this.foo         = function(i){ log+=i.x+"foo"; return i;     }
-
-	on(this)
-	this.on('on:input', function(on){
-		log += "onon"
+	on( '*', (i,o) => {
+		if( o.error ) called = true
 	})
-	this.on('foo:output', (input) => {
-		log += "clone"+input.x
+
+	let a = (i) => ({...i, a:1})
+	let b = (i) => ({...i, b:1}) 
+	let c = (i) => ({...i, c:1}) 
+
+	plug.foo.bar.on([
+		a, 
+		b, 
+		c
+	])
+
+	let out = plug.foo.bar({x:1})
+
+	if( !out.a || !out.b || !out.c ) return error("not piped :(")
+
+	next()
+})
+
+t.add('business rule engine', (next,error) => {
+	let plug = mock()
+	let databaseRules = () => [
+		{expr:"price > 1", exprtype:"filtrex", action:"send email", config:{to:"me@foo"}}
+	]
+
+	on(plug)                                 // init
+
+
+	let exprtype = {filtrex: (i, expr) => filtrex(expr)(i) }
+	let actions  = {"send email": (i)  => console.log("sending mail: "+JSON.stringify(i)) }
+
+	on( '*', (i,o) => {
+		databaseRules().map( (r) => { 
+			if( exprtype[r.exprtype](i, r.expr) ){
+				actions[ r.action ]( Object.assign(o,r.config) )
+			}	
+		})
 	})
-	let out = this.foo(input)
-	if( log != "onon1fooclone1" ) return error("something wrong: "+log)
 
-	this.on.remove()
+	plug.foo.bar.on( (o) => o ) // add listener 
 
-	next()
-})
+	plug.foo.bar({plugin:'db',op:'create',table:'product',price:10})	
 
-t.add("test import expression rules from db",  async (next, error) => {
-	// testdummies
-	var input  = {type:"product", price:2}
-	testlog    = "" 
-
-    on(this)
-	// set() is basically this.foo.bar.flop = ... without crashing (mkdir -p style)
-
-	this.set('email.send'       , (input)       => console.log("    | sending email!")                   )
-	this.set('db.create'        , (input)       => { input.id = 123; this.db.create.succes(input) })
-	this.set('db.create.succes' , (input)       => console.log("    | db item created! "            )    )
-    this.set('foo.bar.flop',  1)
-
-    // we use filtrex,  but you can include your own expression engine here (json-logic-js for form generation e.g.)
-	let dofiltrex = (input, rule) => filtrex(rule.when, {extraFunctions:this})(input)
-
-	let db = [{	
-		id:   "2k2lk34", 
-	    on:   "db.create.succes:input", 
-		when: "id > 0", 
-		then: [ {f:"email.send", opts:{to:"john"}}, {f:"flop"}  ]
-	}]
-
-	this.on.load(db, dofiltrex )
-
-    /*
-    this.on('email.send:output', function logemail(){
-       console.log("logemail") 
-    })
-    .when('foo > 2')
-    
-    this.on('email.send:output', function foolkasdjkf(){
-       console.log("logemail2") 
-    })
-    */
-
-	let out = this.db.create(input)
-	if( !testlog.match(/created/) || !testlog.match(/email/) )
-	    return error("wrong: "+testlog)
-
-	var dump = this.on.serialize()
-	console.log("dump")
-	this.on.ls()
+	// output: sending mail: {"plugin":"db", "op":"create", "table":"product", "price":10, "bar":1, "to":"me@foo"}
 
 	next()
 })
 
-t.add("test import expression rules from db 2",  async (next, error) => {
-	// testdummies
-	var input  = {type:"product", price:2}
-	testlog    = "" 
-
-    on(this)
-	// set() is basically this.foo.bar.flop = ... without crashing (mkdir -p style)
-
-	this.set('email.send'       , (input)       => console.log("    | sending email!")                   )
-	this.set('db.create'        , (input)       => { input.id = 123; this.db.create.succes(input) })
-	this.set('db.create.succes' , (input)       => console.log("    | db item created! "            )    )
-    this.set('foo.bar.flop',  1)
-
-    // we use filtrex,  but you can include your own expression engine here (json-logic-js for form generation e.g.)
-	let dofiltrex = (input, rule) => filtrex(rule.when, {extraFunctions:this})(input)
-
-	let db = [{	
-		id:   "2k2lk34", 
-	    on:   "db.create.succes:input", 
-		when: "id > 0", 
-		then: [ {f:"email.send", opts:{to:"john"}}, {f:"flop"}  ]
-	}]
-
-	this.on.load(db, dofiltrex )
-
-    this.on('email.send:output', function logemail(){
-       console.log("logemail") 
-    })
-    .when('foo > 2')
-    
-    this.on('email.send:output', function foolkasdjkf(){
-       console.log("logemail2") 
-    })
-
-    this.on.error = (msg) => console.log("errorrr")
-
-	let out = this.db.create(input)
-	if( !testlog.match(/created/) || !testlog.match(/email/) || !testlog.match(/errorrr/) )
-	    return error("wrong: "+testlog)
-
-	var dump = this.on.serialize()
-	this.on.ls()
-    console.log( this.on.dot() )
-
-	next()
-})
-
-t.run( () => console.log("done") )
+t.run()
